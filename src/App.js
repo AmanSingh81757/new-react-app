ceb07a7f69ff8593335090b4e8da63f24f4d66d0
Aman Singh <amansingh81757@gmail.com>
2023-04-11 23:13:31 +0530

    minor changes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 // import logo from './logo.svg';
 import './App.css';
+import AddVideos from './components/AddVideos';
 
 function App() {
   return (
     <div className="App">
-      Hello World
+      <AddVideos></AddVideos>
+      <div className='Video'>
+        <img style={{height: '200px', width: '300px' }} src='https://images.unsplash.com/photo-1680925723180-ff3cb559c9b4?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1169&q=80'></img>
+        <div>Channel Name 1M ago 100Views
+        </div>
+      </div>
     </div>
   );
 }
---
+++ src/components/AddVideos.js
@@ -0,0 +1,9 @@
+export default function AddVideos(){
+    return (
+        <form>
+            <input type="text"></input>
+            <input type="text"></input>
+            <button onClick={()=>console.log('a')}>Add video</button>
+        </form>
+    )
+}
