ae73bfa2d54a752d353a6a5d68b9507af2376c29
Aman Singh <amansingh81757@gmail.com>
2023-04-13 23:23:18 +0530

    BS

--- src/components/AddVideos.js
+++ src/components/AddVideos.js
@@ -1,9 +1,27 @@
-export default function AddVideos(){
+import {useState} from 'react'
+
+
+function AddVideos(){
+
+    const [video, setVideo] = useState({
+        time: '1 year ago',
+        channel: 'channel',
+
+    });
+
+    function handleSubmit(e){
+        e.preventDefault();
+    }
+    function handleChange(event){
+        setVideo({...video, [event.target.name]: event.target.value})
+    }
+
     return (
         <form>
-            <input type="text"></input>
-            <input type="text"></input>
-            <button onClick={()=>console.log('a')}>Add video</button>
+            <input type="text" placeholder="title" onChange={handleChange}></input>
+            <input type="text" placeholder="views" onChange={handleChange}></input>
+            <button onClick={handleSubmit}>Add video</button>
         </form>
     )
 }
+export default AddVideos;
---
+++ src/components/Video.js
@@ -0,0 +1,9 @@
+export default function Video(){
+    return (
+        <div className='Video'>
+            <img style={{height: '200px', width: '300px' }} src='https://images.unsplash.com/photo-1680925723180-ff3cb559c9b4?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1169&q=80'></img>
+            <div>Video ka naam</div>
+            <div>Channel Name 1M ago 100Views</div>
+        </div>
+    )
+}
